5cbcac1689123a3c4b85717f744d8ecb0662a816
mohamedalidridii <mohamedali.dridi1@esprit.tn>
2022-07-09 19:01:22 +0200

    migration finished

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import React from "react"
 import Head from "next/head"
 import Script from "next/script"
-import * as PIXI from 'pixi.js'
 export default function Home() {
+  
   return (
     <div>
-    <Script src='https://cdnjs.cloudflare.com/ajax/libs/pixi.js/5.0.2/pixi.min.js'></Script>  
+    <Script src='js/scripts.js'></Script>
+    <Script src='https://cdnjs.cloudflare.com/ajax/libs/pixi.js/5.0.2/pixi.min.js' strategy="beforeInteractive"></Script>  
     <Head>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
@@ -13,12 +14,12 @@ export default function Home() {
         <title>dandelion</title>
     </Head>
     
-    <Script src='js/scripts.js'></Script>
+    
     <div>
       <h1 className="Logo">dandelion <p className="Logo-p">by Mariem</p></h1>
       <canvas className="view" />
     </div>
-
+    
     </div>
 
   )
